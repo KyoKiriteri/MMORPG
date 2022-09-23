afbc05ece678d1a81a27a8a1986bf6c866954faf
KyoKiriteri <sirppu04@gmail.com>
2022-09-23 10:23:36 +0300

    Entered the script - Player.cs

--- MMORPG_Game/Assets/Scripts/Player.cs
+++ MMORPG_Game/Assets/Scripts/Player.cs
@@ -6,16 +6,25 @@ namespace mmorpg
 {
     public class Player : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public static Player instance;
+
+        [SerializeField] private int questID;
+        [SerializeField] private int questName;
+        [SerializeField] private int questDesc;
+        [SerializeField] private int questReward;
+        [SerializeField] private int questEXP;
+        [SerializeField] private int questClear;
+
+        public int QuestID { get => questID; set => questID = value; }
+        public int QuestName { get => questName; set => questName = value; }
+        public int QuestDesc { get => questDesc; set => questDesc = value; }
+        public int QuestReward { get => questReward; set => questReward = value; }
+        public int QuestEXP { get => questEXP; set => questEXP = value; }
+        public int QuestClear { get => questClear; set => questClear = value; }
+
+        private void Awake()
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            instance = this;
         }
     }
 }
