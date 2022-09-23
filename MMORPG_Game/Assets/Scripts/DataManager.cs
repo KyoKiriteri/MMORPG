0acb6038cd839861607f39ebcec16fc58d1e0464
KyoKiriteri <sirppu04@gmail.com>
2022-09-23 10:31:06 +0300

    Third Commit - DataManger.cs

--- MMORPG_Game/Assets/Scripts/DataManager.cs
+++ MMORPG_Game/Assets/Scripts/DataManager.cs
@@ -8,16 +8,26 @@ namespace mmorpg
 {
     public class DataManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private TMP_InputField outputArea;
 
+        public Player player;
+
+        public void GetData()
+        {
+            outputArea.text = null;
+            string uri = "https://localhost:7214/mmorpg";
+            Quest quest = new Quest();
+            StartCoroutine(quest.LoadQuestDataFromDatabase(uri, outputArea));
         }
 
-        // Update is called once per frame
-        void Update()
+        public void PutData()
         {
-
+            outputArea.text = "Loading...";
+            string uri = "https://localhost:7214/mmorpg/1";
+            Quest quest = new Quest(player.QuestID, player.QuestName, 
+                player.QuestDesc, player.QuestReward, 
+                player.QuestEXP, player.QuestClear);
+            StartCoroutine(Quest.SaveQuestDataToDatabase(uri, outputArea));
         }
     }
 }
